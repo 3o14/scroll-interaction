4ac42b27523a41c5d46ea2715db4267c6318693c
3o14 <dldnjswn4265@gmail.com>
2023-07-16 15:26:39 +0900

    새로고침 시 현재 섹션 인식:

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 (() => {
-
-    let yOffset = 0; // window.scrollY 대신 쓸 변수
-    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 높이의 합
-    let currentScence = 0; // 현재 활성화된 섹션 인덱스
+  let yOffset = 0; // window.scrollY 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 높이의 합
+  let currentScence = 0; // 현재 활성화된 섹션 인덱스
 
   // 한 scroll-section에 담을 정보 리스트
   const scenceInfo = [
@@ -55,8 +54,18 @@
         i
       ].objs.container.style.height = `${scenceInfo[i].scrollHeight}px`;
     }
-    console.log("window.innerHeight: ", window.innerHeight);
-    console.log(scenceInfo);
+    
+    let totalScrollHeight = 0;
+    // 아무 곳에서 새로고침 했을 때 현재 currentScence 설정
+    for(let i = 0; i < scenceInfo.length; i++) {
+        totalScrollHeight += scenceInfo[i].scrollHeight;
+        if (totalScrollHeight >= yOffset) {
+            currentScence = i;
+            break;
+        }
+    }
+
+    document.body.setAttribute("id", `show-scene-${currentScence}`);
   }
 
   function scrollLoop() {
@@ -66,21 +75,26 @@
     }
 
     if (yOffset > prevScrollHeight + scenceInfo[currentScence].scrollHeight) {
-        currentScence++;
-        console.log(currentScence);
+      currentScence++;
+      console.log(currentScence);
+      //   document.body.setAttribute("id", `show-scene-${currentScence}`);
     }
 
     if (yOffset < prevScrollHeight) {
-        if(currentScence == 0) return;
-        currentScence--;
+      if (currentScence == 0) return;
+      currentScence--;
+      //   document.body.setAttribute("id", `show-scene-${currentScence}`);
     }
+
+    document.body.setAttribute("id", `show-scene-${currentScence}`);
   }
 
-  window.addEventListener("resize", setLayout);
   window.addEventListener("scroll", () => {
     yOffset = window.scrollY;
     scrollLoop();
   });
 
-  setLayout();
+  // setLayout() : 각 scroll-section 높이 설정
+  window.addEventListener("resize", setLayout);
+  window.addEventListener("load", setLayout); // 이미지나 영상 등의 파일이 화면에 로드 된 후에 setLayout을 실행하기 위함
 })();
