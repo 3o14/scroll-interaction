7ed49071a859bd7c8d8bbbc4155bfe41b794be8d
3o14 <dldnjswn4265@gmail.com>
2023-07-16 22:45:02 +0900

    스크롤 인터랙션 opacity, transform

--- js/main.js
+++ js/main.js
@@ -22,7 +22,12 @@
       },
       values: {
         // css값 정리
-        messageA_opacity: [0, 1, { start: 0.1, end: 0.2 }], // messageA의 투명도의 시작값, 종료값 | {부분 구간 시작, 종료 지점}
+        messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }], // messageA의 css의 시작값, 종료값 | {부분 구간 시작, 종료 지점}
+        messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+        messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }], // translate 타이밍은 opacity와 같아야함
+        messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }], 
+        // 20은 해당 객체의 전체 높이를 100이라고 쳤을 때 20%만큼 이동한다는 의미
+
         messageB_opacity: [0, 1, { start: 0.3, end: 0.4 }],
         messageC_opacity: [0, 1, { start: 0.5, end: 0.6 }],
         messageD_opacity: [0, 1, { start: 0.7, end: 0.8 }],
@@ -93,18 +98,27 @@
       const partScrollEnd = values[2].end * scrollHeight;
       const partScrollHeight = partScrollEnd - partScrollStart;
 
-      if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd){ // 현재 스크롤 값이 애니메이션 분기 내라면
+      if (
+        currentYOffset >= partScrollStart &&
+        currentYOffset <= partScrollEnd
+      ) {
+        // 현재 스크롤 값이 애니메이션 분기 내라면
         // 현재 애니메이션 내에서 얼마나 스크롤 됐는지의 값
-        rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
-      } else if (currentYOffset < partScrollStart) { // 애니메이션 전이라면
-        rv = values[0]; // css 애니메이션 시작값 
-      } else if (currentYOffset > partScrollEnd) { // 애니메이션 후라면
+        rv =
+          ((currentYOffset - partScrollStart) / partScrollHeight) *
+            (values[1] - values[0]) +
+          values[0];
+      } else if (currentYOffset < partScrollStart) {
+        // 애니메이션 전이라면
+        rv = values[0]; // css 애니메이션 시작값
+      } else if (currentYOffset > partScrollEnd) {
+        // 애니메이션 후라면
         rv = values[1]; // css 애니메이션 종료값
       }
     } else {
+      rv = scrollRatio * (values[1] - values[0]) + values[0];
     }
 
-    rv = scrollRatio * (values[1] - values[0]) + values[0];
     return rv;
   }
 
@@ -112,16 +126,38 @@
     const objs = scenceInfo[currentScence].objs;
     const values = scenceInfo[currentScence].values;
     const currentYOffset = yOffset - prevScrollHeight; // 현재 scene에서의 위치 (얼마나 스크롤 됐는지)
+    const scrollHeight = scenceInfo[currentScence].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
 
     console.log(currentScence);
     switch (currentScence) {
       case 0:
-        let messageA_opacity_in = calcValues(
-          values.messageA_opacity,
+        const messageA_opacity_in = calcValues(
+          values.messageA_opacity_in,
           currentYOffset
         );
-        objs.messageA.style.opacity = messageA_opacity_in;
-        console.log(messageA_opacity_in);
+        const messageA_opacity_out = calcValues(
+          values.messageA_opacity_out,
+          currentYOffset
+        );
+        const messageA_translateY_in = calcValues(
+          values.messageA_translateY_in,
+          currentYOffset
+        );
+        const messageA_translateY_out = calcValues(
+          values.messageA_translateY_out,
+          currentYOffset
+        );
+
+        if (scrollRatio <= 0.22) {
+          // in
+          objs.messageA.style.opacity = messageA_opacity_in;
+          objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`;
+        } else {
+          // out
+          objs.messageA.style.opacity = messageA_opacity_out;
+          objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`;
+        }
         break;
 
       case 1:
